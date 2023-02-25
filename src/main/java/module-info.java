294186dd7552368bedb07eefe6a25e1f38748f05
grigoriliev <grigor@grigoriliev.com>
2023-02-26 01:18:32 +0200

    juife-swing: upgrade to Java 11

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module com.grigoriliev.jsampler.juife.swing {
+	requires com.grigoriliev.jsampler.juife;
+
+	requires java.desktop;
+	requires java.logging;
+
+	exports com.grigoriliev.jsampler.juife.swing;
+	exports com.grigoriliev.jsampler.juife.swing.plaf;
+	exports com.grigoriliev.jsampler.juife.swing.wizard;
+}
